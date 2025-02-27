a200e0069d6129c53129ac6a6fcfc2340db66c1c
aidandegooyer <aidandegooyer@gmail.com>
2025-02-27 12:29:47 -0500

    init

---
+++ cc-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+
+function App() {
+  return (
+    <>
+      <Theme accentColor="orange" radius="small">
+        <h1>Collegiate Counter</h1>
+      </Theme>
+    </>
+  );
+}
+
+export default App;
