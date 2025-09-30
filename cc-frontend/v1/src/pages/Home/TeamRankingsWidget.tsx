65c6b709accb12cad0fb230f740958f65dc20394
aidandegooyer <aidandegooyer@gmail.com>
2025-09-30 16:51:51 -0400

    team rank widget fix

--- cc-frontend/v1/src/pages/Home/TeamRankingsWidget.tsx
+++ cc-frontend/v1/src/pages/Home/TeamRankingsWidget.tsx
@@ -1,33 +1,65 @@
 import Logo from "@/components/Logo";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Spinner } from "@/components/ui/shadcn-io/spinner";
-import type { PublicTeam } from "@/services/api";
-import { usePublicTeams } from "@/services/hooks";
+import type { PublicRankingItem } from "@/services/api";
+import { usePublicRankingItems, usePublicRankings } from "@/services/hooks";
 import { ChevronRight } from "lucide-react";
 import { NavLink } from "react-router";
 
 function TeamRankingsWidget() {
-  const { data, isLoading, error } = usePublicTeams(
+  const {
+    data: rankingsData,
+    isLoading: rankingsIsLoading,
+    error: rankingsError,
+  } = usePublicRankings({
+    season_id: import.meta.env.VITE_CURRENT_SEASON_ID,
+    sort: "date",
+    order: "desc",
+    page_size: 1,
+  });
+
+  const {
+    data: rankingItems,
+    isLoading: rankingItemsIsLoading,
+    error: rankingItemsError,
+  } = usePublicRankingItems(
     {
-      sort: "elo",
-      order: "desc",
-      season_id: import.meta.env.VITE_CURRENT_SEASON_ID,
-      page: 1,
+      ranking_id: rankingsData?.results[0]?.id || "",
+      sort: "rank",
+      order: "asc",
       page_size: 10,
     },
     {
-      staleTime: 1000 * 60 * 5,
+      enabled: !!rankingsData?.results[0]?.id,
     },
   );
 
-  if (error) {
+  if (!rankingsData || rankingsData.results.length === 0) {
     return (
       <div className="rankings-widget h-103 rounded-xl border-2 px-4 py-2">
         <NavLink
-          to="/rankings#player"
+          to="/rankings#team"
           className="group flex cursor-pointer items-center justify-between"
         >
-          <h2>Player Rankings</h2>
+          <h2>Team Rankings</h2>
+          <ChevronRight className="text-muted-foreground mr-2 h-6 w-6 transition-all group-hover:mr-0" />
+        </NavLink>
+        <hr />
+        <div>
+          <Spinner className="mx-auto my-4" />
+        </div>
+      </div>
+    );
+  }
+
+  if (rankingsError || rankingItemsError) {
+    return (
+      <div className="rankings-widget h-103 rounded-xl border-2 px-4 py-2">
+        <NavLink
+          to="/rankings#team"
+          className="group flex cursor-pointer items-center justify-between"
+        >
+          <h2>Team Rankings</h2>
           <ChevronRight className="text-muted-foreground mr-2 h-6 w-6 transition-all group-hover:mr-0" />
         </NavLink>
         <hr />
@@ -41,14 +73,19 @@ function TeamRankingsWidget() {
       </div>
     );
   }
-  if (isLoading || !data) {
+  if (
+    rankingsIsLoading ||
+    !rankingsData ||
+    rankingItemsIsLoading ||
+    !rankingItems
+  ) {
     return (
       <div className="rankings-widget h-103 rounded-xl border-2 px-4 py-2">
         <NavLink
-          to="/rankings#player"
+          to="/rankings#team"
           className="group flex cursor-pointer items-center justify-between"
         >
-          <h2>Player Rankings</h2>
+          <h2>Team Rankings</h2>
           <ChevronRight className="text-muted-foreground mr-2 h-6 w-6 transition-all group-hover:mr-0" />
         </NavLink>
         <hr />
@@ -70,7 +107,7 @@ function TeamRankingsWidget() {
       </NavLink>
       <hr />
       <ul className="space-y-3 py-2 pt-3">
-        {data.results.map((team, i) => (
+        {rankingItems.results.map((team, i) => (
           <RankingItem key={team.id || i} index={i} team={team} />
         ))}
       </ul>
@@ -80,7 +117,7 @@ function TeamRankingsWidget() {
 
 interface RankingItemProps {
   index: number;
-  team: PublicTeam;
+  team: PublicRankingItem;
 }
 
 function RankingItem(props: RankingItemProps) {
@@ -92,13 +129,13 @@ function RankingItem(props: RankingItemProps) {
         <div className="flex items-center space-x-2">
           <span className="mr-3 w-4 text-end font-mono">{rank}</span>
           <Logo
-            src={props.team.picture}
+            src={props.team.team?.picture || undefined}
             className="h-6 w-6 rounded-sm"
             alt="pfp"
             type="team"
           />
           <div className="max-w-[128px] truncate overflow-ellipsis whitespace-nowrap transition-colors group-hover:text-blue-400">
-            {props.team.name}
+            {props.team.team.name}
           </div>
         </div>
       </NavLink>
