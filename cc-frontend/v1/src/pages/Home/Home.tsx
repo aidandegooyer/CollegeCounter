a4f8d8b23e2f30ee3163651613c50dce44cac5b1
aidandegooyer <aidandegooyer@gmail.com>
2025-11-19 13:45:47 -0500

    mobile site improvement (?)

--- cc-frontend/v1/src/pages/Home/Home.tsx
+++ cc-frontend/v1/src/pages/Home/Home.tsx
@@ -4,37 +4,41 @@ import PlayerRankingsWidget from "./PlayerRankingsWidget";
 import ResultsWidget from "./ResultsWidget";
 import TeamRankingsWidget from "./TeamRankingsWidget";
 import UpcomingMatchesWidget from "./UpcomingMatchesWidget";
+import c4_logo from "@/assets/c4 title noborder.svg";
+import { NavLink } from "react-router";
 
 function Home() {
   document.title = "College Counter";
   return (
     <div className="app-container mx-4 mt-2 flex justify-center">
       <div className="home w-full max-w-[1200px]">
-        <div
-          className="my-4 flex h-32 w-full items-center justify-center rounded-xl border-2"
-          style={{
-            backgroundPosition: "center", // Change to "top", "bottom", "left", "right", or "center" as needed
-            backgroundSize: "cover", // "cover" fills the div, "contain" fits the image, or use specific values
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          <h1 className="text-center text-6xl text-white">
-            Welcome to Fall 2025!
-          </h1>
-        </div>
-        <div className="flex w-full">
+        <NavLink to="/events">
+          <div className="my-4 flex h-32 w-full flex-col items-center justify-center rounded-xl border-2">
+            <img src={c4_logo} alt="C4 Logo" className="-m-3 h-24" />
+
+            <h1 className="flex items-center text-3xl">
+              Returns for December!
+            </h1>
+          </div>
+        </NavLink>
+        <div className="hidden w-full sm:flex">
           <div className="mr-8 hidden flex-1 space-y-4 lg:block">
             <TeamRankingsWidget />
             <PlayerRankingsWidget />
           </div>
           <div className="flex-2">
-            <NewsWidget />
+            <NewsWidget post_count={8} />
           </div>
           <div className="ml-8 hidden flex-1 space-y-4 md:block">
             <LiveMatchesWidget />
             <UpcomingMatchesWidget />
             <ResultsWidget />
           </div>
+        </div>
+        <div className="flex w-full flex-col justify-center space-y-4 sm:hidden">
+          <UpcomingMatchesWidget limit={2} />
+          <ResultsWidget limit={2} />
+          <NewsWidget post_count={2} />
         </div>
       </div>
     </div>
